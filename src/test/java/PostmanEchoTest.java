7bd2e3c9a07708aca6c5570ac37fc3daf77acc1e
Aleksandr Tkachenko <gknow1990@yandex.ru>
2023-03-22 19:59:12 +0300

    TestSuccessfull

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -9,7 +9,7 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Hi")
+                .body("Hey!")
                 .when()
                 .post("/post")
                 .then()
